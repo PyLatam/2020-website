2b62be13855893787e6cd04b6a6410d9788a0b9f
Paulo <commonzenpython@gmail.com>
2019-02-21 21:44:56 -0500

    Added pricing redirects

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 from cms.sitemaps import CMSSitemap
 
@@ -11,6 +12,8 @@ urlpatterns = [
     path('sitemap.xml', sitemap,
          {'sitemaps': {'cmspages': CMSSitemap}},
          name='django.contrib.sitemaps.views.sitemap'),
+    path('acerca-de/precios/', RedirectView.as_view(url='/precios/', permanent=True)),
+    path('en/about/pricing/', RedirectView.as_view(url='/en/pricing/', permanent=True)),
 ]
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
