de12e084bfc62f6cbb7bc3731a9cf637fca95dc4
Paulo <commonzenpython@gmail.com>
2019-06-22 11:31:11 +0100

    Expose level on admin

--- src/sponsors/admin.py
+++ src/sponsors/admin.py
@@ -6,4 +6,4 @@ from .models import Sponsor
 @admin.register(Sponsor)
 class SponsorAdmin(admin.ModelAdmin):
     list_display = ['name', 'level', 'position']
-    fields = ['position', 'name', 'website', 'contact', 'logo', 'is_active']
+    fields = ['name', 'level', 'position', 'website', 'contact', 'logo', 'is_active']
