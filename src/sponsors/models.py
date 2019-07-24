148134cf6aeb54ae3da577eed88e3a7d82caffa1
Paulo <commonzenpython@gmail.com>
2019-07-24 17:29:49 -0400

    Added multiple tiers per sponsor

--- src/sponsors/models.py
+++ src/sponsors/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import get_language, ugettext_lazy as _
 
 from filer.fields.image import FilerImageField
 
@@ -19,6 +19,19 @@ SPONSORSHIP_LEVELS = Choices(
 )
 
 
+class Tier(models.Model):
+    name_es = models.CharField(max_length=120)
+    name_en = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def name(self):
+        language = get_language()
+        return getattr(self, 'name_{}'.format(language))
+
+
 class Sponsor(models.Model):
     name = models.CharField(max_length=200, unique=True)
     website = models.URLField(blank=False)
@@ -35,6 +48,7 @@ class Sponsor(models.Model):
         choices=SPONSORSHIP_LEVELS,
         default=SPONSORSHIP_LEVELS.basic,
     )
+    tiers = models.ManyToManyField(Tier)
 
     class Meta:
         ordering = ('position',)
@@ -45,6 +59,7 @@ class Sponsor(models.Model):
 
 class SponsorsListPlugin(CMSPlugin):
     level = models.CharField(max_length=12, choices=SPONSORSHIP_LEVELS)
+    tier = models.ForeignKey(Tier, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
-        return self.level
+        return self.tier.name
